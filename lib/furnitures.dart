0cead2dcd828f27b9e0c7bcf8f48e9bbe23d1118
Ana Carolina Hernandes <anacbhernandes@gmail.com>
2020-12-31 12:33:58 -0300

    movendo o json da main para um arquivo proprio

---
+++ lib/furnitures.dart
@@ -0,0 +1,51 @@
+final List furnitures = [
+  {
+    "title": "Mesa",
+    "price": 300,
+    "image": "movel1.jpeg",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+  },
+  {
+    "title": "Cadeira",
+    "price": 120,
+    "image": "movel2.jpg",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+  },
+  {
+    "title": "Manta",
+    "price": 200,
+    "image": "movel3.jpg",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+  },
+  {
+    "title": "Sofá Cinza",
+    "price": 800,
+    "image": "movel4.jpg",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+  },
+  {
+    "title": "Mesa de cabeceira",
+    "price": 400,
+    "image": "movel5.jpg",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+  },
+  {
+    "title": "Jogo de Cama",
+    "price": 250,
+    "image": "movel6.jpg",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+  },
+  {
+    "title": "Sofá Branco",
+    "price": 900,
+    "image": "movel7.jpg",
+    "description":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+  }
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:aluramoveis/furnitures.dart';
 import 'package:aluramoveis/color_swatch.dart';
 import 'package:aluramoveis/pages/cart.dart';
 import 'package:aluramoveis/widgets/custom_appbar.dart';
@@ -44,57 +45,6 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  final List furnitures = [
-    {
-      "title": "Mesa",
-      "price": 300,
-      "image": "movel1.jpeg",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    },
-    {
-      "title": "Cadeira",
-      "price": 120,
-      "image": "movel2.jpg",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    },
-    {
-      "title": "Manta",
-      "price": 200,
-      "image": "movel3.jpg",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    },
-    {
-      "title": "Sofá Cinza",
-      "price": 800,
-      "image": "movel4.jpg",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    },
-    {
-      "title": "Mesa de cabeceira",
-      "price": 400,
-      "image": "movel5.jpg",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    },
-    {
-      "title": "Jogo de Cama",
-      "price": 250,
-      "image": "movel6.jpg",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    },
-    {
-      "title": "Sofá Branco",
-      "price": 900,
-      "image": "movel7.jpg",
-      "description":
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean aliquam libero id mauris mollis convallis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
-    }
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
